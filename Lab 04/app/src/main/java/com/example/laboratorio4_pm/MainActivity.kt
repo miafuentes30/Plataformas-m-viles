0433ac83bd280cd4fb12328ef0d70796aca4c6b9
miafuentes30 <fue23775@uvg.edu.gt>
2024-08-05 08:54:36 -0600

    "Merge" a main

---
+++ Lab 04/app/src/main/java/com/example/laboratorio4_pm/MainActivity.kt
@@ -0,0 +1,125 @@
+package com.example.laboratorio4_pm
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.laboratorio4_pm.ui.theme.Laboratorio4PMTheme
+import androidx.compose.ui.layout.ContentScale
+
+@Composable
+fun MyBox() {
+    val image: Painter = painterResource(id = R.mipmap.uvglogo)
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .border(10.dp, Color(0xFF1c4a27))
+    ) {
+        Image(
+            painter = image,
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxSize()
+                .graphicsLayer(alpha = 0.3f),
+            contentScale = ContentScale.Crop
+        )
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Spacer(modifier = Modifier.height(250.dp))
+            Text(
+                text = "Universidad del Valle de Guatemala",
+                color = Color.Black,
+                fontSize = 30.sp,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center
+            )
+            Spacer(modifier = Modifier.height(30.dp))
+            Text(
+                text = "Programación de plataformas móviles, Sección 30",
+                color = Color.Black,
+                fontSize = 23.sp,
+                textAlign = TextAlign.Center
+            )
+
+            Spacer(modifier = Modifier.height(30.dp))
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 4.dp),
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = "Integrantes ",
+                    color = Color.Black,
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Right
+                )
+                Text(
+                    text = "Mia Fuentes\nAnggelie Velásquez\nLuis Pedro Lira",
+                    color = Color.Black
+                )
+            }
+
+            Spacer(modifier = Modifier.height(30.dp))
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 4.dp),
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = "Catedratico",
+                    color = Color.Black,
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Right
+                )
+                Text(
+                    text = "Juan Carlos Durini",
+                    color = Color.Black
+                )
+            }
+
+            Spacer(modifier = Modifier.height(30.dp))
+            Text(
+                text = "Mia Fuentes\n23775",
+                color = Color.Black,
+                fontSize = 18.sp,
+                textAlign = TextAlign.Center
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MyBoxPreview() {
+    Laboratorio4PMTheme {
+        MyBox()
+    }
+}
